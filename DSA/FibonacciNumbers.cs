1ada9aafc94b26ef535d72251538e77cdf7e3aed
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-10-11 03:18:33 +0300

    Calculation of the Fibonacci numbers in two ways.

---
+++ DSA/FibonacciNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DSA
+{
+    /// <summary>
+    /// <para>This class implements the count Fibonacci numbers.<para>
+    /// </summary>
+    static class FibonacciNumbers
+    {
+        /// <summary>
+        ///     <para>
+        ///         Linear method of counting the number Fibonacci.
+        ///         T(n) = O(n)
+        ///         M(n) = O(1)
+        ///     <para>
+        /// </summary>
+        /// <param name="n">Number of sequence element</param>
+        public static int Get(int n)
+        {
+            int previousNumber = 1; //F(0)
+            int currentNumber = 1; //F(1)
+
+            if (n <= 1)
+                return 1;
+
+            for(int i = 2; i <= n; ++i)
+            {
+                int temp = currentNumber;
+                currentNumber += previousNumber; //F(i)
+                previousNumber = temp; //F(i - 1)
+            }
+
+            return currentNumber;
+
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         Recursive method of counting the number Fibonacci.
+        ///         T(n) = Ω(1.6^n)
+        ///         M(n) = O(n)
+        ///     <para>
+        /// </summary>
+        /// <param name="n">Number of sequence element</param>
+        public static int GetRecursively(int n)
+        {
+            if (n <= 1)
+                return 1;
+            return GetRecursively(n - 1) + GetRecursively(n - 2);
+        }
+    }
+}
