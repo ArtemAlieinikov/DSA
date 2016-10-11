aed4ce6e527903827415cbc1b5e1513f8dc2efc6
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-10-11 03:08:32 +0300

    Linear, binary search, and binary search recursive version have been added.

---
+++ DSA/SearchAlgorithms.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    /// <summary>
+    /// <para>This class includes a variety of search algorithms.<para>
+    /// </summary>
+    class SearchAlgorithms
+    {
+        /// <summary>
+        ///     <para>
+        ///         T(n) = O(n)
+        ///         M(n) = O(1)
+        ///     <para>
+        /// </summary>
+        /// <param name="array">Array to search</param>
+        /// <param name="searchValue">searchValue</param>
+        public static int LinearSearch(int[] array, int searchValue)
+        {
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i] == searchValue)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         T(n) = O(n)
+        ///         M(n) = O(1)
+        ///     <para>
+        /// </summary>
+        /// <param name="array">Array to search</param>
+        /// <param name="searchValue">searchValue</param>
+        public static bool HasElementLinear(int[] array, int searchValue)
+        {
+            for (int i = 0; i < array.Length; ++i)
+                if (array[i] == searchValue)
+                    return true;
+
+            return false;
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         T(n) = θ(n)
+        ///         M(n) = O(1)
+        ///     <para>
+        /// </summary>
+        /// <param name="array">Array to search</param>
+        public static int MaxElementLinear(int[] array)
+        {
+            int maxElement = array[0];
+
+            for (int i = 1; i < array.Length; ++i)
+            {
+                if (maxElement < array[i])
+                { 
+                    maxElement = array[i];
+                }
+            }
+
+            return maxElement;
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         T(n) = O(log n)
+        ///         M(n) = O(log n)
+        ///     <para>
+        /// </summary>
+        /// <param name="array">Array to search</param>
+        /// <param name="element">The element that is looking</param>
+        /// <param name="firstIndex">First index of the array</param>
+        /// <param name="lastIndex">First index of the array</param>
+        public static int BinarySearchRecursively(int[] array, int element, int firstIndex, int lastIndex)
+        {
+            //if (array[firstIndex] > array[lastIndex])
+            //    return -1;
+
+            int middle = (lastIndex + firstIndex) / 2;
+
+            if(lastIndex - firstIndex == 1)
+            {
+                if (array[lastIndex] == element)
+                    return lastIndex;
+                else if (array[firstIndex] == element)
+                    return firstIndex;
+                else
+                    return -1;
+            }
+
+            if (element == array[middle])
+                return middle;
+
+            if (element > array[middle])
+                return BinarySearchRecursively(array, element, middle, lastIndex);
+            else
+                return BinarySearchRecursively(array, element, firstIndex, middle);
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         T(n) = O(log n)
+        ///         M(n) = O(1)
+        ///     <para>
+        /// </summary>
+        /// <param name="array">Array to search</param>
+        /// <param name="element">The element that is looking</param>
+        public static int BinarySearch(int[] array, int element)
+        {
+            int first = 0;
+            int last = array.Length - 1;
+
+            while(last - first != 1)
+            {
+                int mid = (last + first) / 2;
+
+                if(array[mid] == element) 
+                    return mid;
+
+                if (element > array[mid])
+                    first = mid;
+                else 
+                    last = mid;
+            }
+
+            if (array[first] == element)
+                return first;
+            else if (array[last] == element)
+                return last;
+            else return -1;
+        }
+    }
+}
