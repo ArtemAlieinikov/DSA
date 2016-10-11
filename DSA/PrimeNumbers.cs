02eb2a5a99cacf640b81f35e7b4ee1a4ad562f8e
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-10-11 03:11:07 +0300

    Check the number on simplicity.

---
+++ DSA/PrimeNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    /// <summary>
+    /// <para>This class checks, whether a number is prime.<para>
+    /// </summary>
+    public static class PrimeNumbers
+    {
+        /// <summary>
+        ///     <para>
+        ///         T(n) = O(sqrt(n))
+        ///         M(n) = O(1)
+        ///     <para>
+        /// </summary>
+        /// <param name="n">The test number</param>
+        public static bool IsPrime(int n)
+        {
+            if (n == 1)
+                return false;
+
+            /*Суть в том, что для проверки на простоту достаточно 
+             * проверить на целочисленное деление проверяемое число 
+             * в диапазоне от 2 до sqrt(n).
+             */
+            for(int i = 2; (i * i) <= n; ++i)
+            {
+                if ((n % i) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
