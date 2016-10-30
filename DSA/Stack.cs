d5f1dc16ee524129def9fd9d9e3996452f96e51f
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-10-30 23:47:05 +0200

    Added the generic class stack. With a standard interface Capacity, Count, Push, Pop, Peek, Clear.

---
+++ DSA/Stack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    class StackTest<T>
+    {
+        int index;
+        T[] buffer;
+
+        public int Capacity 
+        {
+            get { return buffer.Length; }
+        }
+
+        public int Count
+        {
+            get { return index < 0 ? 0 : index + 1; }
+        }
+
+        public StackTest(int size)
+        {
+            if (size <= 0) { size = 1; }
+            else { }
+
+            index = -1;
+            buffer = new T[size];
+        }
+
+        public void Push(T item)
+        {
+            if (index < buffer.Length - 1)
+            {
+                buffer[++index] = item;
+            }
+            else
+            {
+                throw new InvalidOperationException("There is no place in stack");
+            }
+        }
+
+        public T Pop()
+        {
+            if(index < 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            else { }
+
+            return buffer[index--];
+        }
+
+        public T Peek()
+        {
+            if(index < 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            else { }
+
+            return buffer[index];
+        }
+
+        public void Clear()
+        {
+            index = -1;
+        }
+    }
+}
