5b6412e0e4cdcd57f83d83a584d125ce1d171fb9
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-10-30 23:51:49 +0200

    Added the generic Queue class based on array. With interface IsEmpty, IsFull, Dequeue, Enqueue.

---
+++ DSA/QueueTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    class QueueTest<T>
+    {
+        T[] buffer;
+        int head, tail;
+
+        public bool IsEmpty
+        {
+            get { return head == tail; }
+        }
+
+        public bool IsFull
+        {
+            get { return ((head - tail + buffer.Length) % buffer.Length == 1); }
+        }
+
+        public QueueTest(int size)
+        {
+            buffer = new T[size + 1];
+            head = 0;
+            tail = 0;
+        }
+
+        public T Dequeue()
+        {
+            T result;
+
+            if(head == tail)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            
+            result = buffer[head++];
+
+            if(head == buffer.Length)
+            {
+                head = 0;
+            }
+
+            return result;
+        }
+
+        public void Enqueue(T item)
+        {
+            if ((head - tail + buffer.Length) % buffer.Length == 1)
+            {
+                throw new InvalidOperationException("Queue is full");
+            }
+
+            buffer[tail++] = item;
+
+            if(tail == buffer.Length)
+            {
+                tail = 0;
+            }
+        }
+    }
+}
