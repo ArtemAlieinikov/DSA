39313ffddc57fac4bd98e68c22437a0e45b76322
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-11-24 12:32:48 +0200

    Test.

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -10,7 +10,14 @@ namespace DSA
     {
         static void Main(string[] args)
         {
+            int[] a = new int[9] { 5, 6, 8, 4, 3, 68, 51, 6, 100};
 
+            SortingAlgorithm.QuickSort(a, 0, a.Length - 1);
+
+            foreach (int item in a)
+            {
+                Console.WriteLine(item);
+            }
         }
-    }  
+    }
 }
