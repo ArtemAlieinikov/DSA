69ed4ffa29e41914975136b106b8b0223ecb39d0
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-11-25 16:52:21 +0200

    Test.

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -12,7 +12,7 @@ namespace DSA
         {
             int[] a = new int[9] { 5, 6, 8, 4, 3, 68, 51, 6, 100};
 
-            SortingAlgorithm.QuickSort(a, 0, a.Length - 1);
+            a = SortingAlgorithm.MergeSort(a);
 
             foreach (int item in a)
             {
